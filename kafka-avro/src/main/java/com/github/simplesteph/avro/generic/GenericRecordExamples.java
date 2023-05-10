38f47df514f7f8d2e59e0b9fad27938018b1fca2
USERSAD\bmiguelr <bruno.miguel.ribeiro@emeal.nttdata.com>
2023-05-10 15:40:35 +0100

    Specific Record in Avro - Hands On

--- kafka-avro/src/main/java/com/github/simplesteph/avro/generic/GenericRecordExamples.java
+++ kafka-avro/src/main/java/com/github/simplesteph/avro/generic/GenericRecordExamples.java
@@ -1,9 +1,84 @@
 package com.github.simplesteph.avro.generic;
 
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.*;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+
+import java.io.File;
+import java.io.IOException;
+
 public class GenericRecordExamples {
 
 
     public static void main(String[] args) {
+
+        // step 0 - define schema
+        Schema.Parser parser = new Schema.Parser();
+        Schema schema = parser.parse("{\n" +
+                "     \"type\": \"record\",\n" +
+                "     \"namespace\": \"com.example\",\n" +
+                "     \"name\": \"Customer\",\n" +
+                "     \"doc\": \"Avro Schema for our Customer\",     \n" +
+                "     \"fields\": [\n" +
+                "       { \"name\": \"first_name\", \"type\": \"string\", \"doc\": \"First Name of Customer\" },\n" +
+                "       { \"name\": \"last_name\", \"type\": \"string\", \"doc\": \"Last Name of Customer\" },\n" +
+                "       { \"name\": \"age\", \"type\": \"int\", \"doc\": \"Age at the time of registration\" },\n" +
+                "       { \"name\": \"height\", \"type\": \"float\", \"doc\": \"Height at the time of registration in cm\" },\n" +
+                "       { \"name\": \"weight\", \"type\": \"float\", \"doc\": \"Weight at the time of registration in kg\" },\n" +
+                "       { \"name\": \"automated_email\", \"type\": \"boolean\", \"default\": true, \"doc\": \"Field indicating if the user is enrolled in marketing emails\" }\n" +
+                "     ]\n" +
+                "}");
+
+        // step 1 - create a generic record
+        GenericRecordBuilder customerBuilder = new GenericRecordBuilder(schema);
+        customerBuilder.set("first_name", "bruno");
+        customerBuilder.set("last_name", "ribeiro");
+        customerBuilder.set("age", 39);
+        customerBuilder.set("height", 185F);
+        customerBuilder.set("weight", 100.0F);
+        customerBuilder.set("automated_email", false);
+
+        GenericData.Record customer = customerBuilder.build();
+        System.out.println(customer);
+
+        // step 2 - write that generic record to a file
+
+        // writing to a file
+        final DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(schema);
+        try (DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(datumWriter)) {
+            dataFileWriter.create(customer.getSchema(), new File("customer-generic.avro"));
+            dataFileWriter.append(customer);
+            System.out.println("Written customer-generic.avro");
+            dataFileWriter.close();
+        } catch (IOException e) {
+            System.out.println("Couldn't write file");
+            e.printStackTrace();
+        }
+
+        // step 3 - read a generic record from a file
+
+        final File file = new File("customer-generic.avro");
+        final DatumReader<GenericRecord> datumReader = new GenericDatumReader<>();
+        GenericRecord customerRead;
+        try (DataFileReader<GenericRecord> dataFileReader = new DataFileReader<>(file, datumReader)){
+            customerRead = dataFileReader.next();
+            System.out.println("Successfully read avro file");
+            System.out.println(customerRead.toString());
+
+            // get the data from the generic record
+            System.out.println("First name: " + customerRead.get("first_name"));
+
+            // read a non existent field
+            System.out.println("Non existent field: " + customerRead.get("not_here"));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // step 4 - interpret as a generic record
 
     }
 }
